3c41d61edb90f74799e94d36cc674a9c986a8014
RailonA <railonacosta@gmail.com>
2021-03-29 18:04:10 -0400

    Add basic event up to number 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
          has_many :events,
-         class_name: "Employee",
+         class_name: "Event",
          foreign_key: :creator_id
 
         
