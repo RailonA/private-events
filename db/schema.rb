80ad12436b6a1afbe671a2c2330ac646d238a04c
ixboy <ismaelixboy@gmail.com>
2021-03-30 04:22:18 +0300

    fix sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_26_222408) do
+ActiveRecord::Schema.define(version: 2021_03_30_011641) do
 
   create_table "events", force: :cascade do |t|
     t.string "event"
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 2021_03_26_222408) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
