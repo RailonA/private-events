924d4f97f9f5e51a6cf5d6978f22b5ae0d55a2a7
RailonA <railonacosta@gmail.com>
2021-04-12 16:06:03 -0400

    Fix some rubocop linters that did not pass

--- config/puma.rb
+++ config/puma.rb
@@ -1,3 +1,5 @@
+# rubocop:disable Style/RedundantFetchBlock
+
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
@@ -36,3 +38,4 @@ pidfile ENV.fetch('PIDFILE') { 'tmp/pids/server.pid' }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+# rubocop:enable Style/RedundantFetchBlock
